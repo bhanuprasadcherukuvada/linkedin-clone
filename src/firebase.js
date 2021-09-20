34da5e359bd09d55c20f0855ec1637b802ed22cc
bhanuprasadcherukuvada <bhanuprasadcherukuvada2000@gmail.com>
2021-09-20 21:08:11 +0530

    linkedin clone base

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 const firebaseConfig = {
-	apiKey: "AIzaSyA2iI9zmybEsPhB945MsVrktg7ZJwGCP64",
-	authDomain: "linkedin-ui-clone-6b4c1.firebaseapp.com",
-	projectId: "linkedin-ui-clone-6b4c1",
-	storageBucket: "linkedin-ui-clone-6b4c1.appspot.com",
-	messagingSenderId: "476018980857",
-	appId: "1:476018980857:web:5361c9daa919bf8293af28",
-	measurementId: "G-EFM55F22Y9"
-  };
+	apiKey: process.env.FIREBASE_APIKEY,
+	authDomain: process.env.FIREBASE_AUTHDOMAIN,
+	projectId: process.env.FIREBASE_PROJECTID,
+	storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+	appId: process.env.FIREBASE_APPID,
+	measurementId: process.env.FIREBASE_MEASUREMENTID,
+};
 
 
   const app = firebase.initializeApp(firebaseConfig);
